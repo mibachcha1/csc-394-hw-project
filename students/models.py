13cec75126f5e71f24ff41ee0ece60db8cbcb58f
mibachcha1 <68137662+mibachcha1@users.noreply.github.com>
2023-01-26 22:14:46 -0600

    HW-10 Completed

--- students/models.py
+++ students/models.py
@@ -1,5 +1,5 @@
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
 
 # Create your models here.
 
@@ -14,7 +14,7 @@ class Student(models.Model):
 class Course(models.Model):
     department = models.CharField(max_length=60)
     number = models.CharField(max_length=3)
-    students = models.ManyToManyField(Student)
+    students = models.ManyToManyField(Student, related_name='student_courses')
 
     def __str__(self) -> str:
         return self.department + "-" + self.number
